342542339f750ba53f4ab8f41cce6fe8593f9d00
annvilla1998 <villalobos1998@gmail.com>
2025-03-31 20:17:49 -0700

    Module 5 completed baseline

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,9 +1,28 @@
-var fs = require('fs');
-var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
-
+const tripsEndpoint = "http://localhost:3000/api/trips";
+const options = {
+    method: "GET",
+    headers: {
+        'Accept': 'application/json'
+    }
+}
 /** GET travel view */
-const travel = (req, res) => {
-    res.render('travel', { title: 'Travlr Getaways', trips });
+const travel = async(req, res) => {
+    await fetch(tripsEndpoint, options)
+    .then(res => res.json())
+    .then(trips => {
+
+        let message = null;
+        if(!(trips instanceof Array)) {
+            message = 'API lookup error';
+            trips = [];
+        } else { 
+            if(!trips.length) {
+                message = "No trips exist in our database!"
+            }
+        }
+        res.render('travel', { title: 'Travlr Getaways', trips, message });
+    })
+    .catch(err => res.status(500).send(e.message));
 };
 
 module.exports = {
